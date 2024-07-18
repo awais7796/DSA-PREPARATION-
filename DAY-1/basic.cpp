506b668f036e582aea302127978779304c44fb8a
awais7796 <awaitzz49@gmail.com>
2024-07-19 00:16:24 +0530

    learned data types if else satement in cpp and practise some ques

---
+++ DAY-1/basic.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // skeleton
+    //    cout<<"hello world";
+
+    // ----------------Conditional Statment-----------
+    // ***************if statement**********
+
+    // Write  a program that takes an input of age and prints if u are an adult or not
+    // Sol
+    // int age;
+    // // cin >> age;
+    // if (age >= 18)
+    // {
+    //     cout << "You are and adult";
+    // }
+    // else
+    // {
+    //     cout << " you are an under age person";
+    // }
+
+ /*prob no.2:-
+ A School has following rules for grading system;
+ a. below 25- F
+ b. 25 to 44- E
+ c. 45 to 49 - D
+ d.  50 to 59 -C
+ e. 60 to 79 - B 
+ f. 80 to 100 -A
+ ask user to enter marks and then prints the corresponding grade.
+ */
+// sol 
+
+// int marks =122;
+//  cin>>marks;
+// if (marks<=25){
+//     cout<<"your grade is F";
+// }else if(marks>=25&&marks<=44){
+//     cout<<"your grade is E";
+// }else if(marks>=45&&marks<=49){
+//     cout<<"your grade is D";
+// }else if(marks>=50&&marks<=59){
+//     cout<<"your grade is C";
+// }else if(marks>=60&&marks<=79){
+//     cout<<"your grade is B";
+// }else if (marks>=80&&marks<=100){
+//     cout<<"your grade is A";
+// }else{
+//     cout<<"Please enter the marks ranging from 1 to 100 ";
+// }
+
+    return 0;
+}
