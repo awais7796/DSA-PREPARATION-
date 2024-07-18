e792557a0365d7d0d79f87ead449efe5992324df
awais7796 <awaitzz49@gmail.com>
2024-07-19 00:23:51 +0530

    some changes has been done

--- DAY-1/basic.cpp
+++ DAY-1/basic.cpp
@@ -2,24 +2,32 @@
 using namespace std;
 int main()
 {
-    // skeleton
+
+/*
+******************NOTES********************************
+1) TIP:->instead of using <iostream> use <bits/stdc++.h> it include the all the library of c++.
+2) "\n" and "endl" used for the next line.
+3) DATATYPES :-int, long,long long, float ,double,boolean, char, string , getline.
+
+
+*/
     //    cout<<"hello world";
 
     // ----------------Conditional Statment-----------
     // ***************if statement**********
 
     // Write  a program that takes an input of age and prints if u are an adult or not
-    // Sol
-    // int age;
-    // // cin >> age;
-    // if (age >= 18)
-    // {
-    //     cout << "You are and adult";
-    // }
-    // else
-    // {
-    //     cout << " you are an under age person";
-    // }
+    //code is below:-
+    int age;
+    // cin >> age;
+    if (age >= 18)
+    {
+        cout << "You are and adult";
+    }
+    else
+    {
+        cout << " you are an under age person";
+    }
 
  /*prob no.2:-
  A School has following rules for grading system;
@@ -31,25 +39,25 @@ int main()
  f. 80 to 100 -A
  ask user to enter marks and then prints the corresponding grade.
  */
-// sol 
+// code  is below:-
 
-// int marks =122;
-//  cin>>marks;
-// if (marks<=25){
-//     cout<<"your grade is F";
-// }else if(marks>=25&&marks<=44){
-//     cout<<"your grade is E";
-// }else if(marks>=45&&marks<=49){
-//     cout<<"your grade is D";
-// }else if(marks>=50&&marks<=59){
-//     cout<<"your grade is C";
-// }else if(marks>=60&&marks<=79){
-//     cout<<"your grade is B";
-// }else if (marks>=80&&marks<=100){
-//     cout<<"your grade is A";
-// }else{
-//     cout<<"Please enter the marks ranging from 1 to 100 ";
-// }
+int marks =122;
+ cin>>marks;
+if (marks<25){
+    cout<<"your grade is F";
+}else if(marks>=25&&marks<=44){
+    cout<<"your grade is E";
+}else if(marks>=45&&marks<=49){
+    cout<<"your grade is D";
+}else if(marks>=50&&marks<=59){
+    cout<<"your grade is C";
+}else if(marks>=60&&marks<=79){
+    cout<<"your grade is B";
+}else if (marks>=80&&marks<=100){
+    cout<<"your grade is A";
+}else{
+    cout<<"Please enter the marks ranging from 1 to 100 ";
+}
 
     return 0;
 }
