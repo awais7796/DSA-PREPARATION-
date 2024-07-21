b9d6bb178efaf078bdfc10bfd24c593f989729b3
awais7796 <awaitzz49@gmail.com>
2024-07-21 11:41:27 +0530

    started with epattern 11

---
+++ Day-3/pattern-2.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(){
+    
+    cout <<"today we will cover the remain 12 pattern problems"
+    
+    
+    
+    
+    
+    
+    return 0;
+}
