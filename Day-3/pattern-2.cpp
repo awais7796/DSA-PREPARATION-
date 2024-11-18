dfa6e0522e5ddab4a4bf569ba64d1a9c8623d33b
awais7796 <awaitzz49@gmail.com>
2024-11-18 10:48:46 +0530

    maths done

--- Day-3/pattern-2.cpp
+++ Day-3/pattern-2.cpp
@@ -258,29 +258,34 @@ for(int i=0;i<n;i++){
 */
 
 void print20(int n){
-  for(int i=0;i<n+2;i++){
-       for(int j=0;j<i;j++){
-           cout<<"* ";
-       }   
-        for(int j=0;j<2*n-1;j++){
-            cout<<" ";
-        }
-       for(int j=i;j<=n;j++){
-           cout<<"* ";
-       }
+    int space=2*n-2;
+   for(int i=1; i <= 2*n-1;i++){
+      int star=i;
+      if(i>n) star=2*n-1;
 
+
+    //   stars 
+     for(int j=1;j<=star;j++){
+         cout<<"*";
+         
+     }
+
+
+// spaces
+    for(int j=1;j<=space;j++){
+    cout<<" ";
+    }
+
+    //  stars 
+     for(int j=1;j<=star;j++){
+         cout<<"*";
+
+     }
     cout<<endl;
-
-
-
-
-
-
-
-
-
-
-}
+    if(i<n) space-=2;
+    else space+=2;
+    
+   }}
 
 
 
